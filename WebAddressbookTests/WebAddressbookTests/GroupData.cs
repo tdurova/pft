ef9819ebc7c658b3ca9782ef10d3139af1d0c164
Tatyana Durova <durova@octonus.com>
2015-09-02 19:59:55 +0300

    Создан вспомогательный класс addNEwContact

---
+++ WebAddressbookTests/WebAddressbookTests/GroupData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+        private string name;
+        private string header = "";
+        private string footer = "";
+
+        //конструктор 1
+        public GroupData(string name)
+        {
+            this.name = name;
+        }
+
+        //конструктор 2
+        public GroupData(string name, string header, string footer)
+        {
+            this.name = name;
+            this.name = header;
+            this.name = footer;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Header
+        {
+            get { return header; }
+            set { name = value; }
+        }
+
+        public string Footer
+        {
+            get { return footer; }
+            set { name = value; }
+        }
+    }
+}
